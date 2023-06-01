0e6a9e8d0ffa1aece1664d8544b906b05f3f928f
nguyentritai96 <nguyentritai96@gmail.com>
2023-06-01 15:27:44 +0700

    comment đệ quy fibonacci

--- math/fibonacci-recursive.js
+++ math/fibonacci-recursive.js
@@ -44,38 +44,38 @@ khởi động n=5: middle 5
   trước n=1: base case 1 -> [f(1) = 1]
   => thoát đệ quy 1, code chạy xuống phía dưới, theo vòng ngược lại
 
-=> chạy code phía dưới cho n=2
-trước n=2: value f(n-1) của n=2 1
+  => chạy code phía dưới cho n=2
+  trước n=2: value f(n-1) của n=2 1
   => đụng đệ quỵ 2, đẩy hết code phía trên đệ quy 2 tiếp theo vào stack cho đến khi đụng base case
-  sau n=0: start 0
-  sau n=0: base case 0 => out đệ quy 2
-trước n=2: value f(n-2) của n=2 0
-trước n=2: result 1 => out vòng n = 2
-
-=> chạy code phía dưới cho n=3
-trước n=3: value f(n-1) của n=3 1
-  => đụng đệ quỵ 2, đẩy hết code phía trên đệ quy 2 tiếp theo vào stack cho đến khi đụng base case
-  sau n=1: start 1
-  sau n=1: base case 1 => out đệ quy 2
-trước n=3: value f(n-2) của n=3 1
-trước n=3: result 2 => out vòng n = 3
-
-=> chạy code phía dưới cho n=4
-trước n=4: value f(n-1) của n=4 2
-  => đụng đệ quỵ 2, đẩy hết code phía trên đệ quy 2 tiếp theo vào stack cho đến khi đụng base case
-  sau n=2: start 2
-  sau n=2: middle 2
-    => đụng đệ quy 1, đẩy hết code phía trên đệ quy 1 tiếp theo vào stack cho đến khi đụng base case
-    trước n=1: start 1
-    trước n=1: base case 1
-  sau n=2: value f(n-1) của n=2 1
-    => đụng đệ quỵ 2, đẩy hết code phía trên đệ quy 2 tiếp theo vào stack cho đến khi đụng base case
     sau n=0: start 0
-    sau n=0: base case 0
-  sau n=2: value f(n-2) của n=2 0
-  sau n=2: result 1 => out đệ quy 2
-trước n=4: value f(n-2) của n=4 1
-trước n=4: result 3 => out vòng n = 4
+    sau n=0: base case 0 => out đệ quy 2
+  trước n=2: value f(n-2) của n=2 0
+  trước n=2: result 1 => out vòng n = 2
+
+  => chạy code phía dưới cho n=3
+  trước n=3: value f(n-1) của n=3 1
+  => đụng đệ quỵ 2, đẩy hết code phía trên đệ quy 2 tiếp theo vào stack cho đến khi đụng base case
+    sau n=1: start 1
+    sau n=1: base case 1 => out đệ quy 2
+  trước n=3: value f(n-2) của n=3 1
+  trước n=3: result 2 => out vòng n = 3
+
+  => chạy code phía dưới cho n=4
+  trước n=4: value f(n-1) của n=4 2
+  => đụng đệ quỵ 2, đẩy hết code phía trên đệ quy 2 tiếp theo vào stack cho đến khi đụng base case
+    sau n=2: start 2
+    sau n=2: middle 2
+     => đụng đệ quy 1, đẩy hết code phía trên đệ quy 1 tiếp theo vào stack cho đến khi đụng base case
+      trước n=1: start 1
+      trước n=1: base case 1
+    sau n=2: value f(n-1) của n=2 1
+    => đụng đệ quỵ 2, đẩy hết code phía trên đệ quy 2 tiếp theo vào stack cho đến khi đụng base case
+      sau n=0: start 0
+      sau n=0: base case 0
+    sau n=2: value f(n-2) của n=2 0
+    sau n=2: result 1 => out đệ quy 2
+  trước n=4: value f(n-2) của n=4 1
+  trước n=4: result 3 => out vòng n = 4
 
 => chạy code phía dưới cho n=5
 khởi động n=5: value f(n-1) của n=5 3
@@ -86,7 +86,7 @@ khởi động n=5: value f(n-1) của n=5 3
     trước n=2: start 2
     trước n=2: middle 2
     trước n=1: start 1
-    trước n=1: base case 1
+    trước n=1: base case 1 -> thoát đệ quy 1
   
     trước n=2: value f(n-1) của n=2 1
       => đụng đệ quy 2
